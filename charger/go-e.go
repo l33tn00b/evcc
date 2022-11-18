08f5bb453cc343ce64891ab8bba1327f2b1f56e9
l33tn00b <daniel.barie@gmx.de>
2022-11-18 12:32:18 +0100

    Update go-e.go
    Sponsor fix.

--- charger/go-e.go
+++ charger/go-e.go
@@ -77,9 +77,9 @@ func NewGoE(uri, token string, cache time.Duration) (api.Charger, error) {
 		c.api = goe.NewLocal(log, util.DefaultScheme(uri, "http"), cache)
 	}
 
-	if !sponsor.IsAuthorized() {
-		return nil, api.ErrSponsorRequired
-	}
+	//if !sponsor.IsAuthorized() {
+	//	return nil, api.ErrSponsorRequired
+	//}
 
 	if c.api.IsV2() {
 		return decorateGoE(c, c.totalEnergy, c.phases1p3p), nil
