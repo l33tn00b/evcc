4d3d0f4b51d2a7a1c12f442fc10f106981d65e63
l33tn00b <daniel.barie@gmx.de>
2022-11-28 15:18:46 +0100

    Update featureflags.js

--- assets/js/featureflags.js
+++ assets/js/featureflags.js
@@ -1,5 +1,6 @@
 export default {
   install: (app) => {
-    app.config.globalProperties.$hiddenFeatures = window.localStorage["hidden_features"] === "true";
+    // app.config.globalProperties.$hiddenFeatures = window.localStorage["hidden_features"] === "true";
+    app.config.globalProperties.$hiddenFeatures = true;
   },
 };
